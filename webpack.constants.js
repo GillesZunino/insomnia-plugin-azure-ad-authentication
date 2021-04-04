ea3d5c647cf7d3a3ab408dc2a493393577825537
Gilles Zunino <gzunino@hotmail.com>
2021-04-03 22:17:32 -0700

    Allow building with webpack or tsdx

---
+++ webpack.constants.js
@@ -0,0 +1,17 @@
+// -----------------------------------------------------------------------------------
+// Copyright 2021, Gilles Zunino
+// -----------------------------------------------------------------------------------
+
+"use strict";
+
+const outputRoot = "dist";
+
+module.exports = {
+    // All outputs are produced under this directory
+    outputRoot: outputRoot,
+
+    // Paths where output are produced - Useful for cleaning generated files
+    outputPaths: [
+        outputRoot
+    ]
+};
---
+++ webpack.dev.js
@@ -0,0 +1,22 @@
+﻿// -----------------------------------------------------------------------------------
+// Copyright 2021, Gilles Zunino
+// -----------------------------------------------------------------------------------
+
+"use strict";
+
+const { merge } = require("webpack-merge");
+
+
+// Common webpack configuration / utilities
+const common = require("./webpack.common.js");
+
+// We are building for development
+const type = common.DEVELOPMENT;
+
+module.exports = merge(common.getCommonConfiguration(type), {
+    mode: type,
+    target: "node",
+    devtool: "inline-source-map",
+    plugins: [
+    ]
+});
