4111b0161d2d766e2e6cbcb789ef5c32744ca10d
Gilles Zunino <gzunino@hotmail.com>
2021-04-03 22:40:25 -0700

    Extract liveDisplayName and run as separate functions

--- src/index.ts
+++ src/index.ts
@@ -17,87 +17,101 @@ function isSendingRequest(context: any): boolean {
 }
 
 
+
+async function pluginMain(context: any, ...args: any[]): Promise<string | null | undefined> {
+  // Configure the Azure AD persistence store to retrieve saved accounts
+  publicClientApplication.ensureStore(context.store);
+
+
+  // Validate arguments
+  const authority: string | undefined = args[0];
+  const tenantId: string | undefined = args[1];
+  const clientId: string | undefined = args[2];
+  const scopes: string | undefined = args[3];
+
+  if (!authority) {
+    throw new Error("'Authority' property is required");
+  }
+
+  //debugger;
+
+  // Tenant ID
+  if (!tenantId) {
+    throw new Error("'Directory (tenant) ID' property is required");
+  }
+  if (!isTenantIdValid(tenantId)) {
+    throw new Error("'Directory (tenant) ID' property is invalid");
+  }
+
+  // Client ID
+  if (!clientId) {
+    throw new Error("'Application (client) ID' property is required");
+  }
+  if (!isClientIdValid(clientId)) {
+    throw new Error("'Application (client) ID' property is invalid");
+  }
+
+  // Scopes
+  if (!scopes) {
+    throw new Error("'Scopes' property is required");
+  }
+  if (!isScopesValid(scopes)) {
+    throw new Error("'Scopes' property must contain at least one valid entry");
+  }
+
+
+  // Apply configuration - This handles cases where the config has not changed
+  const configurationChanged: boolean = await publicClientApplication.configure(authority, tenantId, clientId);
+
+  // First, try to acquire a token silently
+  const normalizedScopes: string[] = normalizeAzureADScopes(scopes);
+  const silentAuthenticationResult: msal.AuthenticationResult | null = await publicClientApplication.authenticateSilent(normalizedScopes);
+  if (silentAuthenticationResult) {
+    return silentAuthenticationResult.accessToken;
+  } else {
+    // Only offer to log in interactively when a request is being sent
+    if (isSendingRequest(context)) {
+      const interactiveAuthenticationResult: msal.AuthenticationResult | null = await publicClientApplication.authenticateInteractive(normalizedScopes);
+      if (interactiveAuthenticationResult) {
+        return interactiveAuthenticationResult.accessToken;
+      } else {
+        // TODO: Error handling
+      }
+    }
+
+    return "Send a request to log in";
+  }
+}
+
+function liveDisplayName(args: any[]): string | null {
+  const htmlArrow = "&rArr;";
+  const currentAuthenticationResult = publicClientApplication?.authenticationResult;
+  if (currentAuthenticationResult) {
+    return `Azure AD ${htmlArrow} [${currentAuthenticationResult.account?.username} - '${currentAuthenticationResult.scopes}']`;
+  } else {
+    return `Azure AD ${htmlArrow} Not logged in [${args[0].value}]`;
+  }
+}
+
+
 const templateTags = [{
   name: "azuread",
   displayName: "Azure AD Token",
   description: "Get an Azure AD access token",
-  liveDisplayName: (args: any[]): string | null => {
-    const htmlArrow = "&rArr;";
-    const currentAuthenticationResult = publicClientApplication?.authenticationResult;
-    if (currentAuthenticationResult) {
-      return `Azure AD ${htmlArrow} [${currentAuthenticationResult.account?.username} - '${currentAuthenticationResult.scopes}']`;
-    } else {
-      return `Azure AD ${htmlArrow} Not logged in [${args[0].value}]`;
-    }
-  },
 
   args: Arguments,
   actions: getActions(publicClientApplication),
 
-  async run(context: any, ...args: any[]): Promise<string | null | undefined> {
-    // Configure the Azure AD persistence store to retrieve saved accounts
-    publicClientApplication.ensureStore(context.store);
-
-    
-    // Validate arguments
-    const authority: string | undefined = args[0];
-    const tenantId: string | undefined = args[1];
-    const clientId: string | undefined = args[2];
-    const scopes: string | undefined = args[3];
-
-    // TODO: authority
-    if (!authority) {
-      throw new Error("'Authority' property is required");
-    }
-
-
-    // Tenant ID
-    if (!tenantId) {
-      throw new Error("'Directory (tenant) ID' property is required");
-    }
-    if (!isTenantIdValid(tenantId)) {
-      throw new Error("'Directory (tenant) ID' property is invalid");
-    }
-
-    // Client ID
-    if (!clientId) {
-      throw new Error("'Application (client) ID' property is required");
-    }
-    if (!isClientIdValid(clientId)) {
-      throw new Error("'Application (client) ID' property is invalid");
-    }
-
-    // Scopes
-    if (!scopes) {
-      throw new Error("'Scopes' property is required");
-    }
-    if (!isScopesValid(scopes)) {
-      throw new Error("'Scopes' property must contain at least one valid entry");
-    }
-  
-    // Apply configuration - This handles cases where the config has not changed
-    await publicClientApplication.configure(authority, tenantId, clientId);
-
-    // First, try to acquire a token silently
-    const normalizedScopes: string[] = normalizeAzureADScopes(scopes);
-    const silentAuthenticationResult: msal.AuthenticationResult | null = await publicClientApplication.authenticateSilent(normalizedScopes);
-    if (silentAuthenticationResult) {
-      return silentAuthenticationResult.accessToken;
-    } else {
-      // Only offer to log in interactively when a request is being sent
-      if (isSendingRequest(context)) {
-        const interactiveAuthenticationResult: msal.AuthenticationResult | null = await publicClientApplication.authenticateInteractive(normalizedScopes);
-        if (interactiveAuthenticationResult) {
-          return interactiveAuthenticationResult.accessToken;
-        } else {
-          // TODO: Error handling
-        }
-      }
-
-      return "Send a request to log in";
-    }
-  }
+  liveDisplayName: liveDisplayName,
+  run: pluginMain
 }];
 
 
 export { templateTags }
+
+
+
+
+
+
+
